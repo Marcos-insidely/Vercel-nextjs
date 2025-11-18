117d37ec68159bac0e311b8b46fb8aab15118bd6
Marcos <marcos@insidely.app>
2025-11-18 17:49:33 +0000

    One more font fix

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,13 +1,11 @@
 import { Inter, Lusitana, Ballet } from '@next/font/google';
 
-export const inter = Inter({ subset: ['latin'] });
+export const inter = Inter({ });
 
 export const lusitana = Lusitana({ 
-    subset: ['latin'],
     weight: ['400', '700']
  });
 
  export const ballet = Ballet({ 
-    subset: ['latin'],
     weight: ['400']
  });
