76a6231834aecf16a989cf5627c676641cb591d9
Marcos <marcos@insidely.app>
2025-11-18 17:11:23 +0000

    Initial updates

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,16 @@
+import SideNav from '@/app/ui/dashboard/sidenav';
+import { Html } from 'next/document';
+ 
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en"><body>
+    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+      <div className="w-full flex-none md:w-64">
+        <SideNav />
+      </div>
+      <div className="grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+    </div>
+    </body>
+    </html>
+  );
+}
---
+++ app/ui/fonts.ts
@@ -0,0 +1,13 @@
+import { Inter, Lusitana, Ballet } from '@next/font/google';
+
+export const inter = Inter({ subset: ['latin'] });
+
+export const lusitana = Lusitana({ 
+    subset: ['latin'],
+    weight: ['400', '700']
+ });
+
+ export const ballet = Ballet({ 
+    subset: ['latin'],
+    weight: ['400']
+ });
